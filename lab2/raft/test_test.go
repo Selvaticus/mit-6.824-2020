34ea9b862515af946e44ab86efafad7f57c9a8b7
Selvaticus <Selvaticus@users.noreply.github.com>
2020-05-20 16:58:15 +0100

    Uncomment test

--- lab2/raft/test_test.go
+++ lab2/raft/test_test.go
@@ -9,7 +9,7 @@ package raft
 //
 
 import "testing"
-// import "fmt"
+import "fmt"
 import "time"
 import "math/rand"
 import "sync/atomic"
@@ -19,36 +19,36 @@ import "sync"
 // (much more than the paper's range of timeouts).
 const RaftElectionTimeout = 1000 * time.Millisecond
 
-// func TestInitialElection2A(t *testing.T) {
-// 	servers := 3
-// 	cfg := make_config(t, servers, false)
-// 	defer cfg.cleanup()
+func TestInitialElection2A(t *testing.T) {
+	servers := 3
+	cfg := make_config(t, servers, false)
+	defer cfg.cleanup()
 
-// 	cfg.begin("Test (2A): initial election")
+	cfg.begin("Test (2A): initial election")
 
-// 	// is a leader elected?
-// 	cfg.checkOneLeader()
+	// is a leader elected?
+	cfg.checkOneLeader()
 
-// 	// sleep a bit to avoid racing with followers learning of the
-// 	// election, then check that all peers agree on the term.
-// 	time.Sleep(50 * time.Millisecond)
-// 	term1 := cfg.checkTerms()
-// 	if term1 < 1 {
-// 		t.Fatalf("term is %v, but should be at least 1", term1)
-// 	}
+	// sleep a bit to avoid racing with followers learning of the
+	// election, then check that all peers agree on the term.
+	time.Sleep(50 * time.Millisecond)
+	term1 := cfg.checkTerms()
+	if term1 < 1 {
+		t.Fatalf("term is %v, but should be at least 1", term1)
+	}
 
-// 	// does the leader+term stay the same if there is no network failure?
-// 	time.Sleep(2 * RaftElectionTimeout)
-// 	term2 := cfg.checkTerms()
-// 	if term1 != term2 {
-// 		fmt.Printf("warning: term changed even though there were no failures")
-// 	}
+	// does the leader+term stay the same if there is no network failure?
+	time.Sleep(2 * RaftElectionTimeout)
+	term2 := cfg.checkTerms()
+	if term1 != term2 {
+		fmt.Printf("warning: term changed even though there were no failures")
+	}
 
-// 	// there should still be a leader.
-// 	cfg.checkOneLeader()
+	// there should still be a leader.
+	cfg.checkOneLeader()
 
-// 	cfg.end()
-// }
+	cfg.end()
+}
 
 func TestReElection2A(t *testing.T) {
 	servers := 3
